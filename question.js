42ced7ad15239275a58ac9b6ce74b1932254cd22
Geamisson Iarle <30708207+geamissoniarle@users.noreply.github.com>
2019-07-16 16:15:05 -0300

    adicionado arquivos do projeto

---
+++ question.js
@@ -0,0 +1,71 @@
+var questions = [{
+	"question": "Quem derrotou Cell?",
+	"option1": "Vegeta",
+	"option2": "kuririn",
+	"option3": "Gohan",
+	"option4": "Goku",
+	"answer": "4"
+}, {
+	"question": "Qual o nome da filha de Kuririn ?",
+	"option1": "Mia",
+	"option2": "Pan",
+	"option3": "Maron",
+	"option4": "Lilly",
+	"answer": "3"
+}, {
+	"question": "Os saiyajins possuem uma cauda que faz com que se transformem em gorilas gigantes chamados de...?",
+	"option1": "Zaru",
+	"option2": "Oozaru",
+	"option3": "Boozaru",
+	"option4": "Bozaru",
+	"answer": "2"
+}, {
+	"question": "Qual o disfarce que gohan passa a usar na tentativa de esconder sua identidade ?",
+	"option1": "Grande Saiyaman",
+	"option2": "saiyajin",
+	"option3": "Grande Saiyamen",
+	"option4": "Saiyaman",
+	"answer": "1"
+}, {
+	"question": "Quais os nomes dos cinco integrantes das Forças Especiais Ginyu ?",
+	"option1": "Capitão Ginyu, Zarbon, Gurdo, Nappa e Yuz",
+	"option2": "Boter, Freeza, Nappa, Ricum e Gurdo",
+	"option3": "Capitão Ginyu, Boter, Gurdo, Yuz e Ricum",
+	"option4": "Capitão Ginyu, Zarbon, Gurdo, Freeza e Yuz",
+	"answer": "3"
+}, {
+	"question": "No planeta do Sr. Kaio a gravidade é aumentada em quantas vezes?",
+	"option1": "100",
+	"option2": "10",
+	"option3": "20",
+	"option4": "200",
+	"answer": "2"
+}, {
+	"question": "Quem destrói o Planeta Maligno?",
+	"option1": "Goku",
+	"option2": "Vegeta",
+	"option3": "Piccolo",
+	"option4": "Gohan",
+	"answer": "4"
+}, {
+	"question": "Quem ensina a tecnica do teletransporte a Goku ?",
+	"option1": "Os Namekuseijins",
+	"option2": "Os Tsufurujins",
+	"option3": "os Yardrats",
+	"option4": "Ninguem,Ele ja nasceu sabendo",
+	"answer": "3"
+}, {
+	"question": "Por qual motivo vegeta se deixou ser controlado por babidi??",
+	"option1": "Para voltar a ser quem era antes",
+	"option2": "Para lutar com Piccolo",
+	"option3": "Para que Babidi pudesse liberar o seu poder oculto e ser mais forte que Goku",
+	"option4": "Para se transformar em super saiyajin",
+	"answer": "3"
+}, {
+	"question": " Em Dragonball SUPER: Qual a nova forma ou técnica que goku obtem no torneio do poder??",
+	"option1": "Supreme Form",
+	"option2": "Limit Break",
+	"option3": "Legendary Super saiyajin",
+	"option4": "Ultra Instinct",
+	"answer": "4"
+}];
